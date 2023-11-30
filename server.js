b741efd9c60e1f96b2307250a4ef740162eaad20
casanaa <antoniocasana@yahoo.com>
2023-11-29 19:00:04 -0600

    started working on server.js file

--- server.js
+++ server.js
@@ -24,8 +24,68 @@ const db = mysql.createConnection;(
 
 db.connect((err) => {
     if (err) throw err;
-    console.log("Employee Tracker");
-    mainMenu();
+    console.log('connected as id ' + connection.threadId);
+    afterConnection();
   });
 
+  afterConnection = () => {
+    console.log("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    console.log("+                            -")
+    console.log("-     EMPLOYEE MANAGEMENT    +")
+    console.log("+           SYSTEM           -")
+    console.log("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    promptUser();
+  };
 
+    inquirer
+      .prompt({
+        type: "list",
+        name: "mainMenu",
+        message: "What would you like do?",
+        choices: [
+          "View all Departments",
+          "View all Roles",
+          "View all Employees",
+          "Add a Department",
+          "Add an Employee",
+          "Update Employee Role",
+          "Exit"
+        ]
+      })
+
+      .then((mainMenu) => {
+        const { choices } = answers; 
+  
+        if (choices === "View all Departments") {
+          showDepartments();
+        }
+  
+        if (choices === "View all Roles") {
+          showRoles();
+        }
+  
+        if (choices === "View all Employees") {
+          showEmployees();
+        }
+  
+        if (choices === "Add a Department") {
+          addDepartment();
+        }
+  
+        if (choices === "Add a Role") {
+          addRole();
+        }
+  
+        if (choices === "Add an Employee") {
+          addEmployee();
+        }
+  
+        if (choices === "Update an employee role") {
+          updateEmployee();
+        }
+  
+        if (choices === "No Action") {
+          connection.end()
+      };
+    });
+  ;
